0e1e844bae5b8da2fad189a7ebbc38b1ebae67fd
Muhammad Faizan Hashmi <82911454+FaizanHashmi44@users.noreply.github.com>
2022-11-15 12:40:13 +0500

    Add files via upload

---
+++ style.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('#btn').click(function(){
+        $('.card').css('display', 'block');
+    });
+    $('.cross').click(function(){
+        $('.card').css('display', 'none');
+    });
+    $('.cancel').click(function(){
+        $('.card').css('display', 'none');
+    });
+    $('.btn2').click(function(){
+        $('.card').css('display', 'none');
+    });
+})
